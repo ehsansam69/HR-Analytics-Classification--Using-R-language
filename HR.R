198904b8156083c4108ae8c77dc65f5029eed50a
Ehsan Sam <ehsan_sm@ymail.com>
2021-01-06 22:55:41 +0330

    EDD

--- HR.R
+++ HR.R
@@ -1,1 +1,13 @@
 df <- read.csv('train.csv',header = TRUE)
+str(df)  #description of data frame 
+
+#uni variate analysis
+summary(df)
+
+
+# suspicious for outlier or skewness ; no_of_trainings  length_of_service  
+
+hist(df$length_of_service)
+hist(df$no_of_trainings)
+
+barplot(table(df$region))
