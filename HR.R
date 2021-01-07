0bb6f933c8089cf80dcfda1d60cf4c4dfbbe7bec
Ehsan Sam <ehsan_sm@ymail.com>
2021-01-07 13:43:50 +0330

    fill na

--- HR.R
+++ HR.R
@@ -5,9 +5,21 @@ str(df)  #description of data frame
 summary(df)
 
 
-# suspicious for outlier or skewness ; no_of_trainings  length_of_service  
+# suspicious for outlier or skewnes ; no_of_training  length_of_service 
+# NA value in previous_year_rating
 
 hist(df$length_of_service)
 hist(df$no_of_trainings)
-
+#categorical values
 barplot(table(df$region))
+barplot(table(df$department))
+barplot(table(df$education))
+barplot(table(df$recruitment_channel))
+
+unique(df$previous_year_rating)
+
+# we decided to fill NA values for previous_year_rating with 0 
+which(is.na(df$previous_year_rating))
+df$previous_year_rating[is.na(df$previous_year_rating)] <- 0
+
+
